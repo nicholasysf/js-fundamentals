eec979c4983e42e4af21692051052bcb76b8cd03
Nicholasysf <nicholasysf@gmail.com>
2022-08-11 00:46:47 +0800

    Starting to learn how to manipulate DOM via JS.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,33 @@
-// first function
-function add7(number) {
-  sum = number + 7;
-  return sum;
-}
+//choose the container u want to put the stuff in 
+const container = document.querySelector("#container");
+// creating a paragraph inside the container
+const para = document.createElement("p");
+//adding class selector to <p></p>
+para.classList.add("para")
+//adding text to the paragraph.
+para.textContent = "Hey I'm Red!";
+//adding this node to the container 
+container.appendChild(para);
 
-console.log(add7(8))
+const header = document.createElement("h3");
+header.classList.add("header");
+header.textContent = "I'm a blue h3";
+container.appendChild(header);
 
-// second function
-function multiply(num1,num2){
-  result = num1 * num2;
-  return result;
-}
+const node1 = document.createElement("div");
+node1.classList.add("node1");
+node1.style.backgroundColor = "pink";
+node1.style.border = "thick solid black"
 
-console.log(multiply(3,4))
+const header2 = document.createElement("h1");
+header2.classList.add("header2");
+header2.textContent = "I'm in a div."
 
-//third function 
-function capitalize(string){
-  return string.charAt(0).toUpperCase() + string.slice(1);
-  
-}
+const para2 = document.createElement("p");
+para2.classList.add("para2");
+para2.textContent = "ME TOO!";
 
-console.log(capitalize("testing this function"))
+node1.appendChild(header2);
+node1.appendChild(para2);
+container.appendChild(node1);
 
-//fourth function
-function lastLetter(myString){
-  return myString.charAt(myString.length-1);
-}
-
-console.log(lastLetter("this is a very long sentencE"))
